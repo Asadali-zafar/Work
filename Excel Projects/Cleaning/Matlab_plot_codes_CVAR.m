387aa4199c755ab6c4d6f2990201fecfbfb9baa7
Assad Ali <asadalizafar@hotmail.com>
2023-12-09 01:16:38 +0500

    data analytics

---
+++ Excel Projects/Cleaning/Matlab_plot_codes_CVAR.m
@@ -0,0 +1,252 @@
+% Sample data
+data = [18.47665848,	20.85218224;
+        17.5413,    	16.87208493;
+        19.5281,    	18.87556;
+        19.92808888,	25.30037;
+        23.69547,   	21.44394;
+        23.55172428,	23.28387102;
+        27.2937,    	23.458387;
+        22.99111,   	26.399199;
+        26.9467,    	26.9467;
+        23.77816,   	23.77816;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('Mega watt');
+title('DG power');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [20.51747221,	19.2037;
+        11.29868,   	14.1612;
+        17.07326,   	16.4969;
+        16.08646,   	10.857;
+        19.16659897,	19.65933;
+        14.33572,   	16.42802;
+        11.375,     	15.7333;
+        15.98529577,   	13.12573462;
+        11.76809,   	11.85669;
+        12.0503,    	12.0503;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('Mega watt');
+title('Battery discharge');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [9.969777689,	11.5202;
+        16.1718716,   	19.0985;
+        9.0709,     	9.9537;
+        14.90154949,   	15.84804261;
+        7.884,      	6.493;
+        11.62238,   	13.5342;
+        8.537203,   	10.06019;
+        10.62426,   	13.53756;
+        7.76327,    	7.76327;
+        11.556152,  	11.556152;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('Mega watt');
+title('Battery charge');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [8.6,	        7.394;
+        9.131928395,	8.5;
+        9,          	7.906923438;
+        9.394,      	6.5976;
+        8.3378,     	7.3923;
+        8,          	7.0693;
+        10,         	9;
+        8.8907,     	7.3907;
+        9.6294,     	9.6294;
+        8,          	8;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('power sold to grid');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [0.891664062,	0.5;
+        0.065,      	0.5;
+        1,          	0.374;
+        0.5,        	0;
+        0.3325,     	1;
+        0.0621,     	0.0621;
+        0,          	0;
+        0.4657,     	0.5;
+        0,          	0;
+        0.2593,     	0.2593;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('power from grid');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [0.178,  	    0.331576512;
+        0.019876908,	0.034466677;
+        0.126053792,	0;
+        0.181949493,	0.137132609;
+        0.372601027,	0.503752754;
+        0.088735717,	0.0375;
+        0,          	0.071865086;
+        0,          	0;
+        0,          	0;
+        0,          	0;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('power shedding');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [1.0468, 	    1.200376512;
+        4.97456,   	    4.97456;
+        1.271622792,	1.1548;
+        5.666668884,	6.13158;
+        1.0768,     	1.453152754;
+        4.319,      	4.19448;
+        5.51156,    	5.51156;
+        6.09036,    	6.09036;
+        1.5844,      	1.5844;
+        6.171968,   	6.171968;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('powercurtailment');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [0,          	0;
+        1.905123092,	1.925;
+        1.663969,   	3.317823438;
+        0,          	0;
+        0,          	0;
+        1.7059,     	1.6684;
+        1.389,      	1.389;
+        1.6848,     	2.383575381;
+        3.614,         	3.614;
+        2.6196,     	2.6196;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('battery reserve discharge');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [1.098435,    	    0.9241;
+        1.46184,        	1.8299112;
+        0,              	0;
+        2.14646,        	3.12946;
+        0,              	0;
+        0.81148,        	1.690591;
+        2.38802,        	2.459885;
+        1.736117,    	    1.60442;
+        0.3766,         	0.3766;
+        2.2084,         	2.2084;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('battery reserve charge');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
+% Sample data
+data = [0.519,  	0.519;
+        0.5,    	0.5;
+        0.5,    	0.519;
+        0,      	0;
+        0.5,    	0.519;
+        1,      	1;
+        0.5,    	0.5;
+        0.5,    	0.5;
+        1.5,    	1.5;
+        1.5,    	1.5;];
+
+% Create the stacked bar chart
+bar(data);
+
+% Add labels and title
+xlabel('Scenerios');
+ylabel('DG reserve');
+
+% Add legend
+legend('Beta 0.2', 'Bata 0.3');
+
